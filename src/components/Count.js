4529f013235109a58f7283cf24777e3e7f19334e
projecto2021 <duanly1987@gmail.com>
2022-02-21 21:02:28 +0100

    Count.css center

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,9 +1,32 @@
+import { useReducer } from "react";
+import "../components/Count.css";
 
-const Count = () =>{
-  return(
-    <div>
-     <h2>Count</h2>
-    </div>
-  )
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENTAR":
+      return { count: state.count + 1 };
+    case "DECREMENTAR":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 }
+
+const Count = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const mas = () => dispatch({ type: "INCREMENTAR" });
+
+  const restar = () => dispatch({ type: "DECREMENTAR" });
+
+  return (
+    <div className="center">
+      <h2>Countado Reducer: {state.count} </h2>
+      <button onClick={mas}>+</button>
+      <button onClick={restar}>-</button>
+    </div>
+  );
+};
 export default Count;
